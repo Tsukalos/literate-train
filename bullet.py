a12246e65ecba9f5705baf54799428593cc33fc2
Tsukalos <pedrolamkowski@gmail.com>
2017-06-26 22:29:27 -0300

    Start of bullet class and bullet patterns

--- bullet.py
+++ bullet.py
@@ -1,7 +1,21 @@
 from entity import *
 from pygame.math import *
 from collections import namedtuple
+import bulletPatterns
 
 class Bullet(Entity):
     def __init__(self,rect,surf):
         Entity.__init__(self,rect,surf)
+        self.timePassed = 0
+        self.movementTimer = 0
+
+    def update(self, timePassed, Player):
+        self.timePassed += timePassed
+
+        updateMovement(Player)
+
+        self.timePassed = 0
+
+    def updateMovement(self, Player):
+        self.movementTimer+=self.timePassed
+        #pos = self.Pattern.update(movementTimer, Player)
