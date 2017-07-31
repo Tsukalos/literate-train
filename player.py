57a789f6833d2e827b9752a0814f5ead2abbd6ac
Tsukalos <pedrolamkowski@gmail.com>
2017-07-31 17:30:19 -0300

    player firing patch mostly functional

--- player.py
+++ player.py
@@ -1,33 +1,41 @@
 from entity import *
-import pygame
+import pygame, bulletpattern
 
 class Player(Entity):
     def __init__(self, rect, surf):
         Entity.__init__(self, rect, surf)
         self.firing = False
         self.name = "Player"
+        self.firingpattern = bulletpattern.PlayerPattern1()
+
 
     def updateRect(self):
         Entity.updateRect(self)
 
-    def update(self, keypress, timePassed):
+    def update(self, keypress, timePassed, bulletList):
         self.timePassed+=timePassed
         self.updateMovement(keypress)
+        if(self.firing):
+            bulletList += self.updateFiring()
         self.updateRect()
         self.animation()
         self.timePassed = 0
 
     def updateMovement(self, keypress):
-        if keypress[K_w]:
+        if keypress[K_UP]:
             self.y -= 3
-        if keypress[K_a]:
+        if keypress[K_LEFT]:
             self.x -= 3
-        if keypress[K_s]:
+        if keypress[K_DOWN]:
             self.y += 3
-        if keypress[K_d]:
+        if keypress[K_RIGHT]:
             self.x += 3
 
         if keypress[K_z]:
-            firing = True
+            self.firing = True
         else:
-            firing = False
+            self.firing = False
+
+    def updateFiring(self):
+        return self.firingpattern.update(self, self.timePassed)
+        pass
