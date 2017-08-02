2a6a99f644b24f07d52b5a3047185c7cb871ce53
Tsukalos <pedrolamkowski@gmail.com>
2017-08-02 18:54:58 -0300

    effects done, minor changes in other files

--- bulletpattern.py
+++ bulletpattern.py
@@ -95,3 +95,29 @@ class Pattern2():
 
 		
 		return bulletlist
+
+class Pattern3():
+	def __init__(self):
+		self.timePassed = 0
+		self.c = 0
+		self.vet = Vector2(1,0) * 10
+		self.surf1 = pygame.image.load("data/bullet.png").convert()
+		self.surf1.set_colorkey((255,255,255))
+		self.mask1 = pygame.mask.from_surface(self.surf1)
+		pass
+
+	def update(self, origin, target, timePassed):
+		self.timePassed += timePassed
+		bulletlist = []
+		if self.timePassed % 2000 != self.timePassed:
+			posx = origin.rect.centerx
+			posy = origin.rect.centery 
+			posx += self.vet.x
+			posy += self.vet.y
+			r = Rect(posx,posy,self.surf1.get_width(),self.surf1.get_height())
+			r.center = (posx, posy)
+			bulletlist.append(bullet.Bullet(r,self.surf1,movement.Line(Vector2(self.vet.x, self.vet.y),0.6,0,6),self.mask1))
+			self.timePassed = 0
+
+		
+		return bulletlist
---
+++ effect.py
@@ -0,0 +1,48 @@
+import pygame.image
+from entity import *
+from pygame.math import *
+
+
+class EffectSimpleSprite():
+    def __init__(self, rect, isRef, surf1, duration, tileSize):
+        self.surf1 = surf1
+        #self.surf1.set_colorkey((255,255,255))
+        self.rect = rect.copy()
+        if isRef:
+            self.ref = rect
+        else:
+            self.ref = None
+        self.rect.w = tileSize.w
+        self.rect.h = tileSize.h
+        self.tileSize = tileSize
+        self.nframes = self.surf1.get_width()/tileSize.w 
+        self.interval = duration/self.nframes
+        self.currentFrame = 0
+        self.end = False
+        self.timePassed = 0
+
+    def update(self, timePassed):
+        if self.ref != None:
+            self.rect.x = self.ref.x
+            self.rect.y = self.ref.y
+        self.timePassed += timePassed
+        if(self.timePassed >= self.interval and (not self.end)):
+            if(self.currentFrame < self.nframes):
+                self.currentFrame+=1
+                self.timePassed = 0
+            else:
+                self.end = True
+    
+    def draw(self, update_list, screen):
+        if not self.end:
+            screen.blit(self.surf1,self.rect,Rect(self.currentFrame*self.tileSize.w,0,self.tileSize.w,self.tileSize.h))
+
+    def clearBg(self, update_list, screen, background):
+        screen.blit(background,self.rect,self.rect)
+        r = self.rect.copy()
+        r.w +=5
+        r.h +=5
+        update_list.append(r)
+        del r
+
+        
